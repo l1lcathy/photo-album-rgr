b42fccde3cde08298b110347bc1953bbb9299cbe
lestnitsa21 <olesya210706@icloud.com>
2026-05-27 19:56:29 +0400

    Add files via upload

---
+++ src/main/java/com/photoalbum/security/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.photoalbum.security;
+
+import com.photoalbum.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .map(AppUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("Пользователь не найден"));
+    }
+}
